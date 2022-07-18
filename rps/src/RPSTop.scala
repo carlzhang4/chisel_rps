58e851a5ae652eea2d7c812e4462d50d21a032c5
carlzhang4 <2470871694@qq.com>
2022-07-18 10:20:44 +0800

    <checkpoint>: add qdma to project
    add qdma XDC constrains
    
    now qdma_latency and hbm compression are both ok

--- rps/src/RPSTop.scala
+++ rps/src/RPSTop.scala
@@ -5,19 +5,22 @@ import chisel3.util._
 import common._
 import common.storage.XAXIConverter
 import hbm._
+import qdma.QDMAPin
+import qdma.QDMA
+import qdma.SimpleAXISlave
+import qdma.AXIB
+import qdma.H2C
+import qdma.H2CLatency
+import qdma.C2HLatency
 
 class RPSTop extends RawModule {
+	val qdma_pin		= IO(new QDMAPin())
 	val sysClkP			= IO(Input(Clock()))
 	val sysClkN			= IO(Input(Clock()))
 	val led		        = IO(Output(UInt(1.W)))
 
 	led         		:= 0.U
 	
-	val ibufdsInst = Module(new IBUFDS)
-	ibufdsInst.io.I		:= sysClkP
-	ibufdsInst.io.IB	:= sysClkN
-	val sysClk 			= ibufdsInst.io.O
-
 	val mmcmTop	= Module(new MMCME4_ADV_Wrapper(
 		CLKFBOUT_MULT_F 		= 12,
 		MMCM_DIVCLK_DIVIDE		= 1,
@@ -26,14 +29,14 @@ class RPSTop extends RawModule {
 		MMCM_CLKIN1_PERIOD 		= 10
 	))
 
-	mmcmTop.io.CLKIN1	:= sysClk
+	mmcmTop.io.CLKIN1	:= IBUFDS(sysClkP,sysClkN)
 	mmcmTop.io.RST		:= 0.U
 
 	val clk_hbm_driver	= mmcmTop.io.CLKOUT0 //100M
-	val userClk		= mmcmTop.io.CLKOUT1 //300M
+	val userClk			= mmcmTop.io.CLKOUT1 //300M
 
-
-    val hbmDriver = withClockAndReset(clk_hbm_driver, false.B) {Module(new HBM_DRIVER(WITH_RAMA=false))}
+	//HBM
+    val hbmDriver 		= withClockAndReset(clk_hbm_driver, false.B) {Module(new HBM_DRIVER(WITH_RAMA=false))}
 	hbmDriver.getTCL()
 	val hbmClk 	    	= hbmDriver.io.hbm_clk
 	val hbmRstn     	= withClockAndReset(hbmClk,false.B) {RegNext(hbmDriver.io.hbm_rstn.asBool)}
@@ -52,4 +55,69 @@ class RPSTop extends RawModule {
 	
 	dontTouch(hbmClk)
 	dontTouch(hbmRstn)
+	//QDMA
+	val qdma 			= Module(new QDMA("202101"))
+	qdma.getTCL()
+	ToZero(qdma.io.reg_status)
+	ToZero(qdma.io.c2h_data.bits)
+	ToZero(qdma.io.h2c_cmd.bits)
+	ToZero(qdma.io.c2h_cmd.bits)
+	qdma.io.h2c_data.ready	:= 0.U
+	qdma.io.c2h_data.valid	:= 0.U
+	qdma.io.h2c_cmd.valid	:= 0.U
+	qdma.io.c2h_cmd.valid	:= 0.U
+
+	qdma.io.pin <> qdma_pin
+	qdma.io.user_clk	:= userClk
+	qdma.io.user_arstn	:= userRstn
+	qdma.io.soft_rstn	:= 1.U
+
+	val axi_slave = withClockAndReset(qdma.io.user_clk,!qdma.io.user_arstn){Module(new SimpleAXISlave(new AXIB))}//withClockAndReset(qdma.io.pcie_clk,!qdma.io.pcie_arstn)
+	axi_slave.io.axi	<> XAXIConverter(qdma.io.axib, qdma.io.pcie_clk, qdma.io.pcie_arstn, qdma.io.user_clk, qdma.io.user_arstn)
+
+	val control_reg = qdma.io.reg_control
+	val status_reg = qdma.io.reg_status
+
+	//H2C
+	val h2c =  withClockAndReset(qdma.io.user_clk,!qdma.io.user_arstn){Module(new H2CLatency())}
+	h2c.io.start_addr			:= Cat(control_reg(100), control_reg(101))
+	h2c.io.burst_length			:= control_reg(102)
+	h2c.io.start				:= control_reg(103)
+	h2c.io.total_words			:= control_reg(104)
+	h2c.io.total_cmds			:= control_reg(105)
+	h2c.io.wait_cycles			:= control_reg(106)
+	
+	h2c.io.count_err_data		<> status_reg(100)
+	h2c.io.count_right_data		<> status_reg(101)
+	h2c.io.count_total_words	<> status_reg(102)
+	h2c.io.count_send_cmd		<> status_reg(103)
+	h2c.io.count_time			<> status_reg(104)
+	status_reg(105)				<> h2c.io.count_latency(31,0)
+	status_reg(106)				<> h2c.io.count_latency(63,32)
+	h2c.io.h2c_cmd				<> qdma.io.h2c_cmd
+	h2c.io.h2c_data				<> qdma.io.h2c_data
+
+	//C2H
+	val c2h = withClockAndReset(qdma.io.user_clk,!qdma.io.user_arstn){Module(new C2HLatency())}
+	c2h.io.start_addr			:= Cat(control_reg(200), control_reg(201))
+	c2h.io.burst_length			:= control_reg(202)
+	c2h.io.offset				:= control_reg(203)
+	c2h.io.start				:= control_reg(204)
+	c2h.io.total_words			:= control_reg(205)
+	c2h.io.total_cmds			:= control_reg(206)
+	c2h.io.wait_cycles			:= control_reg(207)
+	c2h.io.pfch_tag				:= control_reg(209)
+	c2h.io.tag_index			:= control_reg(210)
+	c2h.io.ack_fire				:= axi_slave.io.axi.w.fire()
+	
+	c2h.io.count_send_cmd		<> status_reg(200)
+	c2h.io.count_send_word		<> status_reg(201)
+	c2h.io.count_time			<> status_reg(202)
+	status_reg(203)				<> c2h.io.count_latency_cmd(31,0)
+	status_reg(204)				<> c2h.io.count_latency_cmd(63,32)
+	status_reg(205)				<> c2h.io.count_latency_data(31,0)
+	status_reg(206)				<> c2h.io.count_latency_data(63,32)
+	c2h.io.count_recv_ack		<> status_reg(207)
+	c2h.io.c2h_cmd			<> qdma.io.c2h_cmd
+	c2h.io.c2h_data			<> qdma.io.c2h_data
 }
--- rps/sv/RPSTop.xdc
+++ rps/sv/RPSTop.xdc
@@ -1,8 +1,6 @@
 set_property C_USER_SCAN_CHAIN 1 [get_debug_cores dbg_hub]
 connect_debug_port dbg_hub/clk [get_nets -hier -filter {NAME =~ */mmcmGlbl/mmcmGlbl_io_CLKOUT0}]
 
-set_false_path -to [get_pins -hier *sync_reg[0]/D]
-
 set_property PACKAGE_PIN D32 [get_ports led]
 set_property IOSTANDARD LVCMOS18 [get_ports led]
 
@@ -20,3 +18,19 @@ set_false_path -from [get_cells hbmRstn_reg]
 
 set_false_path -from [get_clocks -of_objects [get_pins hbmDriver/mmcmGlbl/mmcm4_adv/CLKOUT0]] -to [get_clocks -of_objects [get_pins hbmDriver/mmcmAxi/mmcm4_adv/CLKOUT0]]
 set_false_path -from [get_clocks -of_objects [get_pins hbmDriver/mmcmGlbl/mmcm4_adv/CLKOUT3]] -to [get_clocks -of_objects [get_pins hbmDriver/mmcmAxi/mmcm4_adv/CLKOUT0]]
+
+#qdma
+create_clock -name sys_clk -period 10 [get_ports qdma_pin_sys_clk_p]
+
+set_false_path -from [get_ports qdma_pin_sys_rst_n]
+set_property PULLUP true [get_ports qdma_pin_sys_rst_n]
+set_property IOSTANDARD LVCMOS18 [get_ports qdma_pin_sys_rst_n]
+set_property PACKAGE_PIN BH26 [get_ports qdma_pin_sys_rst_n]
+set_property CONFIG_VOLTAGE 1.8 [current_design]
+
+set_property LOC [get_package_pins -of_objects [get_bels [get_sites -filter {NAME =~ *COMMON*} -of_objects [get_iobanks -of_objects [get_sites GTYE4_CHANNEL_X1Y7]]]/REFCLK0P]] [get_ports qdma_pin_sys_clk_p]
+set_property LOC [get_package_pins -of_objects [get_bels [get_sites -filter {NAME =~ *COMMON*} -of_objects [get_iobanks -of_objects [get_sites GTYE4_CHANNEL_X1Y7]]]/REFCLK0N]] [get_ports qdma_pin_sys_clk_n]
+set_false_path -from [get_cells -regexp {qdma/axil2reg/reg_control_[0-9]*_reg\[.*]}]
+set_false_path -to [get_cells -regexp {qdma/axil2reg/reg_status_[0-9]*_reg\[.*]}]
+set_false_path -to [get_pins -hier *sync_reg[0]/D]
+set_false_path -to [get_cells qdma/reports_*_reg]
